3d5d2c71e24d79e37851dd5db6e2b248ef723b42
Vladimir <Kajdashov@gmail.com>
2020-12-16 21:23:33 +0300

    Lesson 1

--- temperature.js
+++ temperature.js
@@ -1,4 +1,15 @@
-// заглушка
 function temperature (){
+    let Tc = parseInt(prompt('Введите температуру в градусах цельсия:'));
+    let Tf = (9 / 5) * Tc + 32;
+    alert('Температура в градусах по фаренгейту: ' + Tf);
+}
 
+function replace (){
+    let name = 'Василий';
+    let admin = name;
+    alert(admin);
+}
+
+function join(){
+    alert(1000 + '108');
 }
